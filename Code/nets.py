889c005659aaa5aa1b82d117ac676c7c2434593f
a-pouplin <amp115@ic.ac.uk>
2017-07-26 14:52:47 +0100

    Script: commented

--- Code/nets.py
+++ Code/nets.py
@@ -1,3 +1,7 @@
+"""
+All current networks to be used for the Generative Adversarial Models
+"""
+
 from lasagne.layers import InputLayer, DenseLayer, Conv2DLayer, Deconv2DLayer, flatten, reshape, batch_norm, Upscale2DLayer
 from lasagne.nonlinearities import rectify as relu
 from lasagne.nonlinearities import LeakyRectify as lrelu
@@ -5,6 +9,24 @@ from lasagne.nonlinearities import sigmoid
 
 
 def get_gen_celebA(nz=100):
+	"""
+	Generative network adapted for the CelebA database
+
+	The generative network is composed of deconvolution layers. 
+	It takes in input a variable from the latent space (matrix).
+	It returns an image (4D tensor). 
+
+	Parameters
+	----------
+	nz: int 
+		Number of variables in the latent space
+
+	Returns
+	---------
+	gen: class 'Layer' instance or a tuple
+		Return the generative network 
+
+	"""
 	gen = InputLayer(shape=(None,nz))
 	gen = DenseLayer(incoming=gen, num_units=1024*4*4)
 	gen = reshape(incoming=gen, shape=(-1,1024,4,4))
@@ -16,6 +38,24 @@ def get_gen_celebA(nz=100):
 	return gen
 
 def get_dis_celebA(nz=100):
+	"""
+	Discriminative network adapted for the CelebA database
+
+	The discriminative network is composed of deconvolution layers. 
+	It takes in input an image (4D tensor).
+	It returns a probability (float from 0 to 1). 
+
+	Parameters
+	----------
+	nz: int 
+		Number of variables in the latent space
+
+	Returns
+	--------
+	dis: class 'layer' instance or tuple
+		Return the discriminative network 
+
+	"""
 	dis = InputLayer(shape=(None,3,64,64))
 	dis = batch_norm(Conv2DLayer(incoming=dis, num_filters=128, filter_size=5,stride=2, nonlinearity=lrelu(0.2),pad=2))
 	dis = batch_norm(Conv2DLayer(incoming=dis, num_filters=256, filter_size=5,stride=2, nonlinearity=lrelu(0.2),pad=2))
