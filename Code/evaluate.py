cfe21dd5dbe8445c2b7ccdc4ef1c892df1fab6f6
ToniCreswell <ac2211@ic.ac.uk>
2017-07-26 16:24:10 +0100

    eval

--- Code/evaluate.py
+++ Code/evaluate.py
@@ -70,7 +70,7 @@ def eval_gen(sample_fn, nz, outDir, mean=0.0, std=1.0, rows=5, cols=5):
 	X = sample_fn(Z)
 
 	montage = montage_RGB(X, rows=rows, cols=cols)
-	imsave((os.path,join(outDir,'montage.png'),montage))
+	imsave((os.path.join(outDir,'montage.png'),montage))
 
 	return montage
 
